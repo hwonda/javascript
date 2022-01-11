b21bef6904d1fc996542b6fe0f192736fbaccccb
hwonda <hwondaa@gmail.com>
2022-01-11 15:09:50 +0900

    [공부]test 파일 업로드한다

---
+++ Programmers/0_test.js
@@ -0,0 +1,19 @@
+/*function meetAt(yyyy,mm,dd){
+    if(yyyy && !mm && !dd){
+        return yyyy+"년"
+    }else if(yyyy && mm && !dd){
+        return yyyy+"년 "+mm+"월"
+    }else if(yyyy && mm && dd){
+        return yyyy+"/"+mm+"/"+dd
+    }
+}
+meetAt(2022)*/
+
+function findSmallestElement(n){
+    let minValue
+    for(let i=0;i<n.length;i++){}
+    return minValue
+}
+
+let arr = [100,200,3,0,2,1];
+findSmallestElement(arr)
